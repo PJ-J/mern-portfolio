baceb695e026ee13ecdc8a975ae9b4f4f658c6cc
PJ Jones <pj.jones4000.com>
2022-08-02 18:07:19 -0500

    styling

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,17 @@
 <script>
-  import Icon from '../components/Icon.svelte'
-
+	import Icon from '../components/Icon.svelte';
 </script>
 
-<main>
-<h1 class="font-shrikhard text-3xl">Welcome to SvelteKit</h1>
-<Icon name="tomato" />
-</main>
-
+<body>
+	<main class="container mx-auto my-6 flex place-content-center">
+		<div class="flex flex-col place-items-center">
+			<h1 class="font-shrikhard text-prettyBig text-offWhite text-center">PJ Jones</h1>
+			<div class="relative bg-offWhite h-36 w-full rounded-lg">
+        <h2 class="absolute top-5 -left-7 font-bebas text-veryBig text-dblue text-center">Developer</h2>
+				<h2 class="absolute top-2 -left-5 font-bebas text-veryBig text-coral text-center">Developer</h2>
+			</div>
+			<Icon name="tomato" width="10rem" />
+			<Icon name="leaf1" height="60rem" class="absolute top-12 -z-10" />
+		</div>
+	</main>
+</body>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,13 +2,34 @@ const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
+		container: {
+			center: true,
+			padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+		},
+		fontSize: {
+			prettyBig: ['120px', {
+				lineHeight: '130px',
+			}],
+			veryBig: ['180px', {
+				lineHeight: '150px',
+			}],
+		},
 		extend: {
 			colors: {
 				'dark': '#171717',
 				'offWhite': '#E7E1C5',
+				'coral': '#F57B7B',
+				'dblue': '#242E63',
 			},
 			fontFamily: {
-				'shrikhard': ['"Shrikhand"', 'cursive']
+				'shrikhard': ['"Shrikhand"', 'cursive'],
+				'bebas': ['"Bebas Neue"', 'cursive'],
 			}
 		}
 	},
